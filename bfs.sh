70457f7caf8f466dbcc79ef4a726e4e3097b8279
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-03-07 09:32:37 +0000

    Variables

--- bfs.sh
+++ bfs.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -ex
+
+# Reset the environment so that we can run the experiments in a clean environment
+set -e
+
+# Change the temporary directory to a non ramfs one to avoid OOM
+export TMPDIR="/tmp"
+
+# Set GraalJVM settings
+export JVMOPTS="-server -Xss256K -XX:PretenureSizeThreshold=512M -XX:MaxNewSize=4G -XX:+UseLargePages -XX:+UseTransparentHugePages -XX:+UseNUMA -XX:+UseTLAB -XX:+ResizeTLAB"
+# Set the classpath for the java tests
+export CLASSPATH=$(find -iname \*.jar | paste -d: -s -)
+
 for g in enwiki-2023 eu-2015; do
     cargo build --release --example bv_bf_visit
     cat $g.graph >/dev/null
